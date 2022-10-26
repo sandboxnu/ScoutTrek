c1b63cfdfb222bfb516350c576962100ee45d1b2
KobeZ123 <kobezhe@gmail.com>
2022-10-25 20:31:27 -0400

    initial commit

--- src/modules/createEvent/Inputs/IntervalInput.tsx
+++ src/modules/createEvent/Inputs/IntervalInput.tsx
@@ -8,7 +8,22 @@ import DefaultInputButton from './components/DefaultInputButton';
 import DateTimeLineItem from './components/DateTimeLineItem';
 import {CalendarList} from 'react-native-calendars';
 import {EventInputProps} from './InputTypes';
+import {ModalProps} from 'ScoutDesign/library/Widgets/Modal/Modal';
 
 const chooseInterval = ({id, Modal, modalProps, questionText}: EventInputProps) => {
   return <div>amazing interval input</div>;
+}
+
+// this component uses the DateInput and TimeInput components 
+const DateAndTime = ({ids, Modals, modalPropsList, questionTexts}: EventInputPropsList) => {
+  return <div>
+
+  </div>
+}
+
+interface EventInputPropsList {
+  ids: string[];
+  Modals: React.FC<ModalProps>[];
+  modalPropsList: ModalProps[];
+  questionTexts: string[];
 }
