654c175ca3a28a1b638fa7c2cea5f9052cb3be12
jaron-cui <jaron.cui@gmail.com>
2022-10-27 21:29:47 -0400

    here

--- src/modules/createEvent/Inputs/IntervalInput1.tsx
+++ src/modules/createEvent/Inputs/IntervalInput1.tsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import { Modal } from "react-native";
 
+type InputMode = 'date' | 'time';
 
+const formatDate = (date: Date, time: Date) => {
+  return `${date.getDate()}/${date.getMonth() +
+    1}/${date.getFullYear()} ${time.getHours()}:${time.getMinutes()}`;
+};
 
 const DateTimeSelector = () => {
   const [date, setDate] = useState(new Date());
+  const [inputMode, setInputMode] = useState<InputMode>('date');
+  const [open, setOpen] = useState(false);
+
+  const onChange = (event: DateTimePickerEvent, input: Date | undefined) => {
+    if (event.type != 'set' || input == undefined) {
+      return;
+    }
+    if (inputMode == 'date') {
+      setDate(input);
+      setInputMode('time');
+    } else {
+      setDate(new Date(formatDate(date, input)));
+      setOpen(false);
+    }
+  }
+
   return (
-    <DateTimePicker onChange={
-      (event, date) => event.type == 'set' && date != undefined && setDate(date)
-    } value={date} />
+    <Modal visible={open}>
+      <DateTimePicker onChange={onChange} value={date} mode={inputMode} />
+    </Modal>
   );
 }
 
