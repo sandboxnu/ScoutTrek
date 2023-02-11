31627636a3d01d732427908f76f5cb9a066c70ee
Jason Cheung <59743969+Jason0429@users.noreply.github.com>
2023-02-11 18:45:38 -0500

    prettified everything

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   tabWidth: 2,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'es5',
+  importOrder: ['^components/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
